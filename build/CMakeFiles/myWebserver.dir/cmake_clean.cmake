96e8216c61aef023117506a35a46893943639e7d
alex <2308623956@qq.com>
2025-03-31 21:52:21 +0800

    first commit

---
+++ build/CMakeFiles/myWebserver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myWebserver.dir/main.cpp.o"
+  "myWebserver"
+  "myWebserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myWebserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
